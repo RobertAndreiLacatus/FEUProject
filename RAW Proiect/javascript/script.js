d9c66c5b33870f82a713fdd70661d0a459e0948d
RobertAndreiLacatus <100790743+RobertAndreiLacatus@users.noreply.github.com>
2022-10-27 23:35:32 +0100

    Add files via upload

---
+++ RAW Proiect/javascript/script.js
@@ -0,0 +1,8 @@
+
+document.getElementById("app").innerHTML=`
+<a href="#TitlulUrmator">Our Pourpose</a>
+<h1 class="titlul">Va voi prezenta si alte servicii</h1>
+<div class="first-text" >
+    In cadrul firmei noastre, dorim sa oferim cea mai buna calitate, de aceea dorim sa va ajutam cat de mult posibil si sa fiti multumiti
+    Mai jos va vom prezenta mai multe idei ce au fost folosite pe parcursul proiectelor.
+</div>`;
